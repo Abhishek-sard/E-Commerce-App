6bc38fe4625a21938f083621682983756b4c9c62
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-08-22 23:06:48 +0545

    push

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1 class="text-3xl font-bold text-blue-600">
+      Hello, Tailwind CSS with Vite!
+    </h1>
+  )
+}
+
+export default App
