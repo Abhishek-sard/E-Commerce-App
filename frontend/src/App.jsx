dffd933824e5bcf71ef29f7bfa577f7a77853ff3
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-08-23 09:30:55 +0545

    push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import Orders from './pages/Orders'
+import Product from './pages/Product'
+import PlaceOrder from './pages/PlaceOrder'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <h1 class="text-3xl font-bold text-red-600">
-      Hello, Tailwind CSS with Vite!
-    </h1>
+   <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+    <Navbar/>
+
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path="/collection" element={<Collection/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path="/orders" element={<Orders/>}/>
+      <Route path='/product/:productId' element={<Product/>}/>
+      <Route path='/place-order' element={<PlaceOrder/>}/>
+
+    </Routes>
+   </div>
   )
 }
 
