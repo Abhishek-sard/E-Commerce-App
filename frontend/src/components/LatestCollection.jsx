80ec3429eeca8a6ff3eca5ed043f69456c54f6a6
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-08-23 22:25:32 +0545

    push

---
+++ frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react'
+import { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from './Title'
+
+const LatestCollection = () => {
+
+    const { products } = useContext(ShopContext);
+    const [latestProducts, setLatestProducts] = useState([]);
+
+
+    useEffect(() => {
+        setLatestProducts(products.slice(0,10));
+    },[])
+
+    // useEffect(() => {
+    //     const filteredProducts = products.filter(product => product.latest);
+    //     setLatestProducts(filteredProducts);
+    // }, [products]);
+
+  return (
+    <div className='my-10'>
+        <div className="text-center py-8 text-3xl">
+            <Title text1={'LATEST'} text2={'COLLECTION'} />
+            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+                Discover the latest trends in our collection and elevate your style with our new arrivals.
+            </p>
+        </div>
+      
+    </div>
+  )
+}
+
+export default LatestCollection
---
+++ frontend/src/components/ProductItems.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { ShopContext } from '../context/ShopContext'
+import { Link } from 'react-router-dom';
+
+const ProductItems = ({ id, image, title, price }) => {
+
+    const { currency } = useContext(ShopContext);
+  return (
+    <Link to={`/product/${id}`} className='text-gray-700 cursor-pointer'>
+        <img src={image} alt={title} className='w-full h-auto' />
+        <h2 className='text-lg font-semibold'>{title}</h2>
+        <p className='text-gray-600'>{currency}{price}</p>
+    </Link>
+  )
+}
+
+export default ProductItems
+
+
+// 1:34
+
+
