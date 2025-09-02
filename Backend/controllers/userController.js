0de1cd9ba0b38be8826d777b45a8bcae209adc57
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-09-02 15:50:22 +0545

    push

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,22 +1,22 @@
-
+import userModel from "../models/userModel";
 
 // Route for user login
-const loginUser = async (req, res) => {
-
-}
-
+const loginUser = async (req, res) => {};
 
 //Route for user register
 const registerUser = async (req, res) => {
-    res.json({msg: " Register API WORKING"});
-    
-}
+  try {
+    const { name, email, password } = req.body;
 
 
+    //checking user already exists or not
+    const exists = await userModel.findOne({email});
+  } catch (error) {
+
+  }
+};
 
 //Route for admin login
-const adminLogin = async (req, res) => {
+const adminLogin = async (req, res) => {};
 
-}
-
-export {loginUser, registerUser, adminLogin}
+export { loginUser, registerUser, adminLogin };
